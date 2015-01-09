2f6dbb0c31052fc0b70226adeaa16c7d6ff5b563
Yehonal <hw.2@hotmail.it>
2015-01-09 15:14:47 +0100

    Updated

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2007 - 2014 Hyperweb2 All rights reserved.
+ * GNU General Public License version 3; see www.hyperweb2.com/terms/
+ */
+
+'use strict';
+
+hw2.define([
+    //'hw2!PATH_JS_LIB:event/include.js', normally already loaded by following libs
+    'hw2!PATH_JS_LIB:uri/Uri.js'
+],function () {
+    var $ = this;
+});
