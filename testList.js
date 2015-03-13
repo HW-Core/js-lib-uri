9de1c27677e22e98ac6f0ea5f8d42d81856be237
Yehonal <hw.2@hotmail.it>
2015-03-13 14:32:21 +0100

    update repositories

---
+++ testList.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2007 - 2014 Hyperweb2 All rights reserved.
+ * GNU General Public License version 3; see www.hyperweb2.com/terms/
+ */
+
+// list of test files
+hw2.define(function () {
+    $ = this;
+    return {
+        dep: [
+            $.const.PATH_JS_LIB + "uri/index.js"
+        ],
+        test: [
+            $.const.PATH_JS_LIB + "uri/tests/tests/uri.js",
+        ]
+    };
+});
